847ff05d61f18c153b82fb87f71a05744cfeb53b
kokoyroy <kokoyroy@gmail.com>
2021-02-05 06:28:48 +0200

    added local and view child refs... for learning purposes

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,12 +6,12 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
 
   @Output('sCreated') serverCreated = new EventEmitter<{ serverName: string, content: string }>();
   @Output() blueprindCreated = new EventEmitter<{ blueprintName: string, content: string }>();
-
+  @ViewChild('serverContent') serverContent: ElementRef;
 
 
   constructor() { }
@@ -24,11 +24,13 @@ export class CockpitComponent implements OnInit {
 
 
 
-  onAddServer() {
-    this.serverCreated.emit({ serverName: this.newServerName, content: this.newServerContent })
+  onAddServer(element: HTMLInputElement) {
+    // console.log(this.serverContent.nativeElement.value);
+
+    this.serverCreated.emit({ serverName: element.value, content: this.serverContent.nativeElement.value })
   }
 
-  onAddBlueprint() {
-    this.blueprindCreated.emit({ blueprintName: this.newServerName, content: this.newServerContent })
+  onAddBlueprint(element: HTMLInputElement) {
+    this.blueprindCreated.emit({ blueprintName: element.value, content: this.serverContent.nativeElement.value })
   }
 }
